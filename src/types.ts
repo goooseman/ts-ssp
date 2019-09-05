909913dd3927dbb33ca9c624ee5c91325fd19c26
Alexander Gusev <goooseman@me.com>
2019-09-05 07:19:06 +0300

    wip(types): 🚧  Creates SspOptions, SspTypes

---
+++ src/types.ts
@@ -0,0 +1,42 @@
+import { OpenOptions } from "serialport";
+
+export type SspType = "nv10usb" | "nv9usb";
+export type SspParity = "none" | "even" | "mark" | "odd" | "space";
+export type SspOptions = {
+  /**
+   * 'npx @serialport/list' to get a list
+   */
+  device: string;
+  /**
+   * Default is nv10usb
+   */
+  type?: SspType;
+  /**
+   * Default is [1, 0, 1]
+   */
+  currencies?: number[];
+  /**
+   * Default is 9600
+   */
+  baudRate?: OpenOptions["baudRate"];
+  /**
+   * Default is 8
+   */
+  dataBits?: OpenOptions["dataBits"];
+  /**
+   * Default is 2
+   */
+  stopbits?: OpenOptions["stopBits"];
+  /**
+   * Default is none
+   */
+  parity?: SspParity;
+  /**
+   * Default is 0
+   */
+  sspID?: number;
+  /**
+   * Default is 0x80
+   */
+  sequence?: number;
+};

b864b4f4d898d02ac49f8f5fc34f94587125fad5
Alexander Gusev <goooseman@me.com>
2019-10-05 10:29:14 +0300

    fix(typescript): 🐛  Absolute paths are not working when it is a project build

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,4 +1,5 @@
-import { SSPType } from "src/types";
+// tslint:disable-next-line: no-relative-imports
+import { SSPType } from "../types";
 import NV10USBCommands from "./NV10USBCommands";
 import NV9USBCommands from "./NV9USBCommands";
 
